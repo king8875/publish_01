792206a7ebf21be9a77577d58e2b1cf37f55b399
Seokyunghwan <dhgh8875@icloud.com>
2024-04-01 12:19:43 +0900

    2024/04/01

---
+++ js/main.js
@@ -0,0 +1,28 @@
+import Lenis from '@studio-freight/lenis';
+
+const lenis = new Lenis();
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+});
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+};
+
+requestAnimationFrame(raf);
+
+
+
+
+
+
+
+
+
+
+
+$('.search-btn').on('click', function () {
+    alert('해당 폰트를 찾을 수 없습니다.')
+});
