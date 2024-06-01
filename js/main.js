503fad10037598bfd7c4c67b3f333c8fa4ba38d5
Seokyunghwan <dhgh8875@icloud.com>
2024-06-01 22:48:43 +0900

    0601

--- js/main.js
+++ js/main.js
@@ -1,29 +1,19 @@
-import Lenis from '@studio-freight/lenis';
-
-const lenis = new Lenis();
-
-lenis.on('scroll', (e) => {
-  console.log(e)
-});
-
-function raf(time) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
-};
-
-requestAnimationFrame(raf);
 
 
 
+$(document).ready(function () {
+  
+  window.addEventListener('scroll', function () {
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop; //현재 스크롤 위치
+    var scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight; //전체 스크롤 값
+    var clientHeight = document.documentElement.clientHeight || document.body.clientHeight; //
+    var scrolled = (scrollTop / (scrollHeight - clientHeight)) * 100;
+
+    var progressBar = document.querySelector('#progress');
+    progressBar.style.display = 'block';
+    progressBar.style.width = scrolled + '%';
+  })
+})
 
 
-
-
-
-
-
-
-$('.search-btn').on('click', function () {
-  alert('해당 폰트를 찾을 수 없습니다.')
-});
 
