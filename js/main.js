779df9204d2ed09d554dbd0b7afc4fc517151394
Seokyunghwan <dhgh8875@icloud.com>
2024-06-03 22:52:45 +0900

    0603

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 
 $(document).ready(function () {
-  
+
   window.addEventListener('scroll', function () {
     var scrollTop = document.documentElement.scrollTop || document.body.scrollTop; //현재 스크롤 위치
     var scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight; //전체 스크롤 값
@@ -15,5 +15,34 @@ $(document).ready(function () {
   })
 })
 
+
+
+// $.ajax({
+//   type: 'GET',
+//   url: 'https://codingapple1.github.io/hello.txt'
+// }).done(function (결과) {
+//   console.log('gogogogo');
+// });
+
+// $.get('https://codingapple1.github.io/hello.txt').done(function (결과) {
+//   console.log(결과)
+// })
+
+
+// var imgloading = new Promise(function (성공, 실패) {
+//   var img = document.querySelector('#test');
+//   img.addEventListener('load', function () {
+//     성공();
+//   });
+//   img.addEventListener('error', function () {
+//     실패();
+//   });
+// });
+
+// imgloading.then(function () {
+//   console.log('good');
+// }).catch(function () {
+//   console.log('bad')
+// });
 
 
