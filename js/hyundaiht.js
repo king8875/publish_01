fee01daf17bec3e7a6b0c623b347be34544a0116
Seokyunghwan <dhgh8875@icloud.com>
2024-07-18 15:19:15 +0900

    0718

---
+++ js/hyundaiht.js
@@ -0,0 +1,30 @@
+// $(document).ready(function() {
+//     const $sections = $('section');
+//     let currentSectionIndex = 0;
+//     let isScrolling = false;
+
+//     function scrollToSection(index) {
+//         if (isScrolling) return;
+//         isScrolling = true;
+//         $('html, body').animate({
+//             scrollTop: $sections.eq(index).offset().top
+//         }, 800, function() {
+//             isScrolling = false;
+//         });
+//     }
+
+//     $(window).on('wheel', function(event) {
+//         if (event.originalEvent.deltaY > 0) {
+//             if (currentSectionIndex < $sections.length - 1) {
+//                 currentSectionIndex++;
+//                 scrollToSection(currentSectionIndex);
+//             }
+//         } else {
+//             if (currentSectionIndex > 0 ) {
+//                 currentSectionIndex--;
+//                 scrollToSection(currentSectionIndex);
+//             }
+//         }
+
+//     });
+// });
