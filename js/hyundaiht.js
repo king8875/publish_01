4d34ac52fa4b6d8b2325e8fffdef5370c1d67084
Seokyunghwan <dhgh8875@icloud.com>
2024-07-29 01:08:51 +0900

    0729

--- js/hyundaiht.js
+++ js/hyundaiht.js
@@ -29,18 +29,43 @@
 //         }
 
 //     });
-// });
+// }
+
+
 
 $(document).ready(function() {
-    const sec04Img = $('.sec04_top_ul li')
+    const sec04Img = $('.sec04_top_ul li');
+    const sections = $('section');
+    let currentSectionIndex = 0;
+    let isAnimating = false;
+
+    const scrollToSection = (index) => {
+        if (index >= 0 && index < sections.length) {
+            isAnimating = true;
+            currentSectionIndex = index;
+            $('html, body').animate({
+                scrollTop: $(sections[index]).offset().top
+            }, 500, function() {
+                isAnimating = false;
+            });
+        }
+    };
+    $(window).on('wheel', function(event) {
+        if (isAnimating) return;
+        
+        if (event.originalEvent.deltaY > 0) {
+            scrollToSection(currentSectionIndex + 1);
+        } else {
+            scrollToSection(currentSectionIndex - 1);
+        }
+    });
+
     sec04Img.on('mouseover', function() {
         $(this).find('.sec04_img_con').css('transform', 'scale(1.05)');
     });
     sec04Img.on('mouseleave', function() {
         $(this).find('.sec04_img_con').css('transform', 'scale(1)');
     });
-
-   
 
    
 
@@ -65,10 +90,9 @@ $(document).ready(function() {
     $('.gnb_li').mouseleave(function() {
         $(this).find('.red_bg').removeClass('red_bg_hover');
         $(this).find('li').removeClass('text_hover');
-
-
-
     });
+
+
 
     
 
