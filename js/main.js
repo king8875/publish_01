0f4fa4b46fc30b188e68cd43be52bf8c13586d75
Seokyunghwan <dhgh8875@icloud.com>
2024-08-13 16:52:13 +0900

    main.html last commit

--- js/main.js
+++ js/main.js
@@ -32,11 +32,12 @@ $(document).ready(function () {
   // });
 
   gsap.to(".scroll", {
-    opacity: 0,
+    opacity: 1,
     duration: 0.5,
     repeat: -1,
     yoyo: true,
-    ease: "power1.inOut"
+    ease: "power1.inOut",
+    delay: 2
   });
 
   gsap.to(".about_item_content01", {
